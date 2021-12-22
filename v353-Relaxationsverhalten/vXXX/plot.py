4591df92fd61b243bc6fd06a452f4bd97f5945b3
Joel Koch <joel.koch@tu-dortmund.de>
2021-12-22 12:00:46 +0100

    plot geplottet

--- v353-Relaxationsverhalten/vXXX/plot.py
+++ v353-Relaxationsverhalten/vXXX/plot.py
@@ -1,21 +1,46 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+mpl.rcParams.update({
+'font.family': 'serif',
+'text.usetex': True,
+'pgf.rcfonts': False,
+'pgf.texsystem': 'lualatex',
+'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
+})
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#f, A1, A2, a, b = np.genfromtxt('../Dateien/Aufgabebc.txt', unpack = True)
+data = np.genfromtxt('../Dateien/Aufgabebc.txt', delimiter=';', names=True)
+#print(data['f'])
+#print(data['A1'])
+#print(data['A2'])
+#print(data['a'])
+#print(data['b'])
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
-
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
+plt.plot(data['f'], data['A1'], label = r'Amplitude $A_1 \:/\:$ V')
+plt.xlabel(r'$f \:/\:$ kHz')
+plt.ylabel(r'Messwert')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig('build/plot1.pdf')
+plt.clf()
+
+plt.plot(data['f'], data['A2'], label = r'Amplitude $A_2 \:/\:$ V')
+plt.xlabel(r'$f \:/\:$ kHz')
+plt.ylabel(r'Messwert')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot2.pdf')
+plt.clf()
+
+plt.plot(data['f'], data['a'], label = r'$a \:/\:$ ms')
+plt.xlabel(r'$f \:/\:$ kHz')
+plt.ylabel(r'Messwert')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot3.pdf')
+plt.clf()
+
+plt.plot(data['f'], data['b'], label = r'$b \:/\:$ ms')
+plt.xlabel(r'$f \:/\:$ kHz')
+plt.ylabel(r'Messwert')
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/plot4.pdf')
+plt.clf()
