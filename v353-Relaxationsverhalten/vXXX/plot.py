fe317730827972c0329ff385b929765a00391903
Joel Koch <joel.koch@tu-dortmund.de>
2022-01-09 15:11:47 +0100

    Korrekturen, v353

--- v353-Relaxationsverhalten/vXXX/plot.py
+++ v353-Relaxationsverhalten/vXXX/plot.py
@@ -22,13 +22,13 @@ print(r'Plots werden durchgegangen')
 #Beginn Plot zu Aufgabenteil a)
 daten = np.genfromtxt('../Dateien/Aufgabea.txt', delimiter=';', names=True)
 
-x1 = daten['U'] +2.5
-x = np.log(x1)
-y = daten['t']
+y1 = daten['U'] +2.5
+y = np.log(y1)
+x = daten['t']
 T = unp.uarray(daten['t'],0.1) #Fehler von t
-U = unp.uarray(x1,0.1) #Fehler von U
+U = unp.uarray(y1,0.1) #Fehler von U
 
-m , b , r ,p ,std =stats.linregress(y,x)
+m , b , r ,p ,std =stats.linregress(x,y)
 M=unp.uarray(m,std)
 B=unp.uarray(b,std)
 
@@ -37,16 +37,22 @@ B=unp.uarray(b,std)
 N = len(y)
 Delta = N * np.sum(y**2) - (np.sum(y))**2
 
-A = (N * np.sum(x * y) - np.sum(y) * np.sum(x)) / Delta
-B = (np.sum(y**2) * np.sum(x) - np.sum(y) * np.sum(x * y)) / Delta
-#print('Steigung für Aufgabenteil a) beträgt:', A)
-#print('y-Achsenabschnitt für Aufgabenteil a) beträgt:', B)
-#print(m*daten['t']+b)
-#print(x)
-#print(m)
-#print(b)
+A_man = (N * np.sum(x * y) - np.sum(y) * np.sum(x)) / Delta
+B_man = (np.sum(y**2) * np.sum(x) - np.sum(y) * np.sum(x * y)) / Delta
 
-plt.plot(y,x, '.', color = 'crimson', label = 'Messwerte')
+sigma_y = np.sqrt(np.sum((y - A_man * x - B_man)**2) / (N - 2))
+A_error = sigma_y * np.sqrt(N / Delta)
+B_error = sigma_y * np.sqrt(np.sum(x**2) / Delta)
+#print('A_error = ',A_error) #Unsicherheit auf der Steigung auf der manuell berechneten Ausgleichsgeraden
+#print('B_error = ', B_error)    #Unsicherheit auf des  y-Achsenabschnitts auf der manuell berechneten Ausgleichsgeraden  
+#print('Steigung für Aufgabenteil a) beträgt:', A)   #Steigung auf der manuell berechneten Ausgleichsgeraden 
+#print('y-Achsenabschnitt für Aufgabenteil a) beträgt:', B)  #y-Achsenabschnitt auf der manuell berechneten Ausgleichsgeraden 
+
+#print(f'RC_a = {-1/M} [\mu s]') #Durch CorveFit ermittelte Zeitkonstante RC
+#print(f'Steigung aus CurveFit: f{M}')   #Durch CurveFit ermittelte Steigung der Ausgleichsgeraden
+#print(f'y-Achsenabschnitt aus CurveFit: {B}')   #Durch CurveFit ermittelter y-Achsenabschnitt der Ausgleichsgeraden
+
+plt.plot(x, y, '.', color = 'crimson', label = 'Messwerte')
 #plt.errorbar(y,x, xerr = stds(T), yerr = stds(unp.log(U/5)), fmt = '.', color = 'crimson', label = 'Messwerte')
 plt.plot(daten['t'], m*daten['t']+b, color = 'mediumblue', label='Fit')
 #plt.plot(x, A*x + B, color = 'mediumblue', label='lineare Regression')
@@ -73,9 +79,8 @@ plt.plot(w, relAmp, '.', color = 'crimson', label = 'Messwerte')
 
 parameters, pcov = curve_fit(f1, w , data['A2']/4.2, sigma=None, p0=0)
 RC_B=unp.uarray(parameters,pcov)
-#print(f'RC_b = {RC_B*10**6} [\mu s]')
-#print(f'RC_b = {parameters*10**3} [\mu s]')
-#plt.xscale('log')
+#print(f'RC_b = {RC_B*10**3} [\mu s]')   #Zeitkonstante RC mit Unsicherheit(wahrscheinlich ohne "*10**3" richtig!)
+#print(f'RC_b = {parameters*10**3} [\mu s]') #Zeitkonstante RC ohne Unsicherheit(wahrscheinlich ohne "*10**3" richtig!)
 xx = np.linspace(w[0], w[19], 20)
 plt.plot(xx, f1(xx,*parameters), color = 'mediumblue', label='Fit')
 plt.legend()
@@ -89,7 +94,7 @@ plt.clf()
 
 #Plot zu Aufgabenteil c)
 
-phi =  data['a']/data['b']
+phi =  data['a']/data['b']  #Phasenverschiebung -> Messwerte sind nicht gut!
 #print(phi)
 plt.plot(data['f'], phi, '.', color = 'dodgerblue', label = r'Messwerte')
 plt.legend()
@@ -101,7 +106,7 @@ plt.clf()
 
 
 
-#Theorieplot
+#Theorieplot (Wie Plot c eigentlich hätte aussehen sollen mit RC = 1.6)
 x = np.linspace(3,60,200)
 y = np.arctan(-x*1.6)
 a=0.3
@@ -113,17 +118,18 @@ plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/Theorieplot.pdf')
 plt.clf()
 
-#Polarplot
+#Polarplot  
 phi2 = np.linspace(0.0, (np.pi)/2.0, 20)
 plt.polar(phi, data['A2'], '.', color = 'darkviolet', label = r'Messwerte')
-plt.polar(phi2, np.cos(phi2), color='navy', label = r'Theoriekurve')
+plt.polar(phi2, np.cos(phi2), color='navy', label = r'Theoriekurve')    #Theoriekurve wird nicht in dem Polarplot angezeigt
 plt.legend(loc='best')
 plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
 plt.savefig('build/plotd.pdf')
 plt.clf()
 
 
-
+#Versuch 2 Plots in einen Polarplot zu bekommen und sich ausgeben zu lassen.
+#X-Server muss vorher gestartet werden.
 
 #fig = plt.figure()
 #ax1 = plt.subplot(121, projection = 'polar')
