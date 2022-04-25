0f5492f0e320e03717850850cef6ef219573d84d
Felix Symma <felix.symma@tu-dortmund.de>
2022-04-25 20:55:56 +0200

    Korrektur fertig

--- 2tes-Semester/v601/plot.py
+++ 2tes-Semester/v601/plot.py
@@ -1,21 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import uncertainties as uc
+import scipy.constants as const
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+difference=np.array(np.zeros(25))
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+def generatediffernce(y):
+    for i in range(25):
+        difference[i]=y[i+1]-y[i]
+    return difference
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+xrt,yrt=np.genfromtxt('data/RT.txt', unpack=True)
+x423,y423=np.genfromtxt('data/423K.txt', unpack=True)
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#plt.plot(xrt[0:25]*((10)/(25)),generatediffernce(yrt),'x',color="orange")
+#plt.plot(x423[0:25]*((10)/(25)),generatediffernce(y423),'x',color="blue")
+plt.plot(xrt[0:25]*((10)/(25)),generatediffernce(yrt),label="Raumtemperatur",color="mediumblue")
+plt.plot(x423[0:25]*((10)/(25)),generatediffernce(y423),label="423 Kelvin",color="m")
+plt.plot(xrt[20]*((10)/(25)),generatediffernce(yrt)[20],'x',color="red")
+plt.legend()
+plt.grid()
+plt.xlabel(r'$U_A/V$')
+plt.ylabel(r'$I_A(U_A+\upDelta U_A)-I_A(U_A)$')
+plt.yticks([])
+plt.tight_layout()
+plt.savefig('build/Differenz.pdf')
+plt.figure()
