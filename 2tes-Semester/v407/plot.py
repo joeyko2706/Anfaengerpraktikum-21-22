a16a8e90ae80f6a9f049ba25bcba8fb7f6d3d708
Joel Koch <joel.koch@tu-dortmund.de>
2022-05-16 18:43:58 +0200

    Korrektur fertig :disguised_face:

--- 2tes-Semester/v407/plot.py
+++ 2tes-Semester/v407/plot.py
@@ -3,12 +3,8 @@ import numpy as np
 from uncertainties import ufloat
 
 
-alpha, I = np.genfromtxt(
-    "data/aufgabe1pPol.txt", unpack=True
-)  # Die Daten für parallele Polarisation Importieren.
-alpha1, I1 = np.genfromtxt(
-    "data/aufgabe1sPol.txt", unpack=True
-)  # Die Daten für senkrechte Polarisation Importieren.
+alpha, I = np.genfromtxt("data/aufgabe1pPol.txt", unpack=True)  # Die Daten für parallele Polarisation Importieren.
+alpha1, I1 = np.genfromtxt("data/aufgabe1sPol.txt", unpack=True)  # Die Daten für senkrechte Polarisation Importieren.
 n = alpha.size
 
 I_e = 0.54 * 10 ** (-3)  # Nullstrom in A
@@ -22,7 +18,7 @@ def pPolarisation(
     w, I
 ):  # Brechungsindex für pPolarisiertes Licht; w = winkel, I = Photostrom
     E = E_e / np.sqrt(I * 10 ** (-6))
-    return (
+    return abs(
         (1 + E)
         / (np.cos(w) * (1 - E))
         * np.sqrt(1 / 2 * (1 + (np.sqrt(2 * w)) ** 2 * ((1 - E) ** 2 / (1 + E) ** 2)))
@@ -63,30 +59,24 @@ winkeltheo = np.linspace(10, 90, 1000)
 # Die folgenden 6 Zeilen sind nur Befehle, um die Mittelwerte und die Tabellen auszugeben
 
 # print("Mittelwert Brechung bei sPol: ", np.round(np.mean(sBrechung),4), "+/-" , np.round(np.std(sBrechung),4)) #Mittelwert des Brechungsindex bei sPolarisiertem Licht
-# print("Mittelwert Brechung bei pPol: ", np.round(np.mean(pBrechung),4), "+/-" , np.round(np.std(pBrechung),4)) #Mittelwert des Brechungsindex bei pPolarisiertem Licht
+print("Mittelwert Brechung bei pPol: ", np.round(np.mean(pBrechung),4), "+/-" , np.round(np.std(pBrechung),4)) #Mittelwert des Brechungsindex bei pPolarisiertem Licht
 # for i in range(n):
-#     print(alpha[i]," & ", I[i]," & ", pBrechung[i]," ;")    #Tabelle für parallele Polarisation
-#     print(alpha1[i]," & ", I1[i]," & ", sBrechung[i]," ;")  #Tabelle für senkrechte Polarisation
-#     i += 1
+    # print(alpha[i]," & ", I[i]," & ", pBrechung[i]," ;")    #Tabelle für parallele Polarisation
+    # print(alpha1[i]," & ", I1[i]," & ", sBrechung[i]," ;")  #Tabelle für senkrechte Polarisation
+    # i += 1
 
 
 plt.plot(alpha, y1, ".", label="Messwerte $\qty{90}{\degree}$$")  # pPol
 plt.plot(
     winkeltheo,
-    pPolTheorie(3.35268, winkeltheo, 0),
-    label="Theoriekurve $\qty{90}{\degree}$",
-)
+    pPolTheorie(3.35268, winkeltheo, 0),label="Theoriekurve $\qty{90}{\degree}$")
 plt.xlabel(r"Winkel $\alpha$ / Grad")
 plt.ylabel(r"$\sqrt{\frac{I_r}{I_e}}$")
 
 plt.plot(alpha1, y2, ".", label="Messwerte $\qty{0}{\degree}$")  # sPol
-plt.plot(
-    winkeltheo,
-    abs(sPolTheorie(3.35268, winkeltheo, 0)),
-    label="Theoriekurve $\qty{0}{\degree}$",
-)
-plt.xlabel(r"Winkel $\alpha$ / Grad")
-plt.ylabel(r"$\sqrt{\frac{I_r}{I_e}}$")
+plt.plot(winkeltheo, abs(sPolTheorie(3.35268, winkeltheo, 0)),label="Theoriekurve $\qty{0}{\degree}$",)
+# plt.xlabel(r"Winkel $\alpha$ / Grad")
+# plt.ylabel(r"$\sqrt{\frac{I_r}{I_e}}$")
 
 plt.xlim(8,92)
 plt.grid()
