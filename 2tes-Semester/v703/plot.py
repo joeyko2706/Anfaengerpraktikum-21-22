b4ea9892d40464cde638b556da5d05519f6f5d66
Joel Koch <joel.koch@tu-dortmund.de>
2022-05-31 15:14:55 +0200

    Data

--- 2tes-Semester/v703/plot.py
+++ 2tes-Semester/v703/plot.py
@@ -1,21 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.constants as const
+import scipy.optimize as op
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+U, I, z = np.genfromtxt("data/ersterStoff.txt", unpack = True)
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
+n = np.sqrt(z)
+plt.errorbar(U, z, yerr = n, fmt = "gx")
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.savefig("build/plot.pdf")
